6335e87a918fe6dd8a74fc387518949e07f6b1d7
Juan Manuel Sardoy <jmsardoy@gmail.com>
2023-04-19 13:10:03 -0300

    remove module config error pyright

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -5,6 +5,7 @@ return {
         typeCheckingMode = "off",
         diagnosticSeverityOverrides = {
           reportMissingImports = "none", -- disable due to pyright not working with pyenv
+          reportMissingModuleSource = "none", -- disable due to pyright not working with pyenv
         }
       },
     },
